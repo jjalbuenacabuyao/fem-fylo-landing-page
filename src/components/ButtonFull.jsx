19b517f980c2b134ee7ff88d4de61f85ff06b55e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-08-17 11:18:06 +0800

    Resized the elements in cta

--- src/components/ButtonFull.jsx
+++ src/components/ButtonFull.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ButtonFull = () => (
-  <button className='bg-gradient-to-r from-accent-cyan to-accent-blue w-full text-sm font-bold font-raleway rounded-full py-3 z-10'>Get Started</button>
+  <button className='btn-full'>Get Started</button>
 )
 
 export default ButtonFull
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,12 +1,12 @@
 import ButtonFull from './ButtonFull';
 
 const CTA = () => (
-  <div className='absolute bottom-[760px] mx-8 md:mx-[4.10rem] bg-dark-blue-intro rounded-lg text-center py-10 px-6'>
-    <h2 className='text-base font-bold'>Get early access today</h2>
-    <p className='pt-5 pb-[1.875rem] leading-5'>
+  <div className='cta'>
+    <h2>Get early access today</h2>
+    <p>
       It only takes minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.
     </p>
-    <input className='rounded-full mb-5 w-full h-[2.75rem] pl-5 text-xs' type="email" name="email" id="email" placeholder='email@example.com'/>
+    <input type="email" name="email" id="email" placeholder='email@example.com'/>
     <ButtonFull />
   </div>
 )
