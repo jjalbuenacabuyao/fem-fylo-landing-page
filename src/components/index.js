1766811263a814e0b9952eb916bd874e8d92fa0b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-09 11:14:56 +0800

    exported Icon component

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Logo from "./Logo";
 import Nav from "./Nav";
 import Illustration from "./Illustration";
 import Button from "./Button";
+import Icon from "./Icon";
 
 export {
   Header,
   Logo,
   Nav,
   Illustration,
-  Button
+  Button,
+  Icon
 }
