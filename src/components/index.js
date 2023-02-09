af5f2ddc125304c4b1a2de820e5551f011f19976
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-09 10:02:44 +0800

    exported illustration

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Logo from "./Logo";
 import Nav from "./Nav";
+import Illustration from "./Illustration";
 
 export {
   Header,
   Logo,
-  Nav
+  Nav,
+  Illustration
 }
