d40ad2f4626738f918a76f3f707e525319afbc20
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-09 17:01:04 +0800

    initial commit

---
+++ src/containers/CTA.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Illustration } from "../components";
+import { descriptionImg, arrow } from "../assets";
+
+const CTA = () => {
+  return (
+    // CTA Section
+    <section className="bg-dark-blue-main flex flex-col gap-14 px-9 pb-40">
+      <Illustration
+        src={descriptionImg}
+        altText="Illustration of 3 persons having conversation"
+      />
+      <div className="flex flex-col gap-5 items-start">
+        <h2 className="font-raleway text-lg font-bold">
+          Stay productive, wherever you are
+        </h2>
+        <p>
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all your file storage needs.
+        </p>
+        <p>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+        <button className="flex items-center gap-2 border-b-2 border-b-accent-cyan pb-1 leading-none text-accent-cyan">
+          <span>See how Fylo works</span>
+          <img src={arrow} alt="arrow right" />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default CTA;
