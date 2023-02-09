2db1f5cbf7a7a63bd9c9c15c03059def63566a37
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-09 10:41:21 +0800

    exported Button component

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import Illustration from "./Illustration";
+import Button from "./Button";
 
 export {
   Header,
   Logo,
   Nav,
-  Illustration
+  Illustration,
+  Button
 }
