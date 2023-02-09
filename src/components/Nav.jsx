0b7c46a1a49491dedf09da5e41ee95f047b76be0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-09 10:41:54 +0800

    used raleway font-family

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <nav>
-      <ul className='flex gap-6'>
+      <ul className='flex gap-6 font-raleway'>
         <li><a href="#">Features</a></li>
         <li><a href="#">Team</a></li>
         <li><a href="#">Sign In</a></li>
