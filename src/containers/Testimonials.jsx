9e39188d58606f1d30827321d8eeab700b15f27b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-10 11:23:01 +0800

    added alt

--- src/containers/Testimonials.jsx
+++ src/containers/Testimonials.jsx
@@ -31,8 +31,13 @@ const Testimonials = () => {
   ];
   return (
     //Testimonials
-    <section className="relative flex flex-col gap-6 bg-dark-blue-main px-12 pb-96">
-      <img aria-hidden="true" src={quotes} alt="" className="absolute left-16 bottom-full w-6" />
+    <section className="relative flex flex-col gap-6 bg-dark-blue-main px-12 pb-72">
+      <img
+        aria-hidden="true"
+        src={quotes}
+        alt="quotes"
+        className="absolute left-16 bottom-full w-6"
+      />
       {testimonials.map((testimonial) => (
         <TestimonialCard
           name={testimonial.name}
