65d4ec4996eb44a8955195f786635a1f24db0e74
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-10 09:55:06 +0800

    created the markup

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {Button} from "../components"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <h2>Get early access today</h2>
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+      <form>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email@example.com"
+        />
+        <Button text="Get Started For Free" />
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
