6f30723483f0736686da55b5283c98c9f1e890d6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-10 10:10:51 +0800

    added styles

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import {Button} from "../components"
+import { Button } from "../components";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>Get early access today</h2>
+    <div className="flex flex-col gap-6 text-center py-12 px-7 bg-dark-blue-intro">
+      <h2 className="font-raleway text-lg font-bold leading-none">
+        Get early access today
+      </h2>
       <p>
         It only takes a minute to sign up and our free starter tier is extremely
         generous. If you have any questions, our support team would be happy to
         help you.
       </p>
-      <form>
+      <form className="flex flex-col gap-6">
         <input
           type="email"
           name="email"
           id="email"
           placeholder="email@example.com"
+          className="py-3 px-8 rounded-full"
         />
         <Button text="Get Started For Free" />
       </form>
