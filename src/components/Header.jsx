b4a3a9041b97151dba13011d0d0ee929fea4919f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-11 06:12:39 +0800

    adjusted paddingX and paddingY

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav'
 
 const Header = () => {
   return (
-    <header className='px-5 py-6 flex justify-between items-center'>
+    <header className='px-20-80 py-24-74 flex justify-between items-center'>
       <Logo className="w-20" />
       <Nav />
     </header>
