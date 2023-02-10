99c93e9120dd099db875c7e6ffdc8a882e34a5cc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-10 09:59:46 +0800

    exported Contact and Footer

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,10 +2,14 @@ import Hero from "./Hero";
 import Features from "./Features";
 import CTA from "./CTA";
 import Testimonials from "./Testimonials";
+import Contact from "./Contact";
+import Footer from "./Footer"
 
 export {
   Hero,
   Features,
   CTA,
-  Testimonials
+  Testimonials,
+  Contact,
+  Footer
 }
