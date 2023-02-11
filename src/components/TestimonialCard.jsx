ff1843ad5e7c34c489f4241ba407e3452e100af9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-11 11:10:40 +0800

    added conditional classname and ::before element on first testimonial

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { quotes } from "../assets";
 
-const TestimonialCard = ({ name, role, comment, src, alt }) => {
+const TestimonialCard = ({ index, name, role, comment, src, alt }) => {
   return (
-    <div className="flex flex-col gap-5 rounded-md bg-dark-blue-testimonial px-5 py-7 text-xs">
+    <div
+      className={`relative flex flex-col gap-20-28 rounded-md bg-dark-blue-testimonial px-20-24 py-28-44 text-12-14 md:col-span-2 ${
+        index == 2 ? "md:col-start-2" : ""
+      }`}
+    >
+      {index == 0 ? (
+        <img
+          aria-hidden="true"
+          src={quotes}
+          alt="quotes"
+          className="absolute left-4 bottom-full w-6"
+        />
+      ) : (
+        <></>
+      )}
       <p>{comment}</p>
       <div className="grid grid-cols-[auto_1fr] grid-rows-2 gap-x-2 leading-none">
         <img
@@ -10,8 +25,8 @@ const TestimonialCard = ({ name, role, comment, src, alt }) => {
           alt={alt}
           className="row-span-2 w-7 self-center rounded-full"
         />
-        <p className="font-bold self-center">{name}</p>
-        <p className="text-[8px] self-center">{role}</p>
+        <p className="self-center font-bold">{name}</p>
+        <p className="self-center text-[8px]">{role}</p>
       </div>
     </div>
   );
