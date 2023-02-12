700c27921716930cd167d6d1f38807b0b2f0f1d9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-12 11:10:07 +0800

    adjusted the grid in xl devices

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -31,12 +31,12 @@ const Footer = () => {
   const socialIcons = ["logo-facebook", "logo-twitter", "logo-instagram"];
 
   return (
-    <footer className="relative flex flex-col gap-10 bg-dark-blue-footer px-36-120 pt-288-360 pb-48-95 md:grid md:grid-cols-2">
+    <footer className="relative flex flex-col gap-10 bg-dark-blue-footer px-36-120 pt-288-360 pb-48-95 md:grid md:grid-cols-2 xl:grid-cols-8">
       <Contact />
-      <Logo className="md:col-span-2" />
-      <div className="flex flex-col gap-5 md:col-span-2">
-        {contactInfo.map((info) => (
-          <div key={info.key} className="grid grid-cols-[auto_1fr] gap-7">
+      <Logo className="md:col-span-2 xl:col-span-8" />
+      <div className="flex flex-col gap-5 md:col-span-2 xl:col-span-5 xl:grid xl:grid-cols-2 xl:grid-rows-[auto_1fr]">
+        {contactInfo.map((info, index) => (
+          <div key={info.key} className={`grid grid-cols-[auto_1fr] gap-7 ${index == 0 ? "xl:row-span-2" : ""}`}>
             <Icon src={info.icon} alt={info.alt} className="h-auto w-5" />
             <p>{info.content}</p>
           </div>
@@ -63,7 +63,7 @@ const Footer = () => {
         ))}
       </ul>
 
-      <ul className="flex items-center justify-center gap-3 text-2xl leading-none md:col-span-2">
+      <ul className="flex items-center justify-center gap-3 text-2xl leading-none md:col-span-2 xl:col-span-1 xl:items-start">
         {socialIcons.map((icon) => (
           <ion-icon key={icon} name={icon}></ion-icon>
         ))}
