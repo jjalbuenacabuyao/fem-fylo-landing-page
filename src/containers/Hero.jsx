1b35419ddaa01467a241eace7dd11a1f71f8b6e8
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-12 10:33:25 +0800

    use responsive gaps

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -5,13 +5,13 @@ import { illustration } from "../assets";
 const Hero = () => {
   return (
     // Hero Section
-    <section className="px-36-350 pt-9 flex flex-col gap-11 lg:pb-36-280">
+    <section className="px-36-350 pt-9 flex flex-col gap-44-60 lg:pb-36-280">
       <Illustration
         src={illustration}
         alt="Illustration of two persons organizing files in a huge folder"
         className="mx-auto"
       />
-      <article className="flex flex-col items-center text-center gap-5">
+      <article className="flex flex-col items-center text-center gap-20-36">
         <h1 className="font-raleway font-bold text-24-38">All your files in one secure location, accessible anywhere.</h1>
         <p>Fylo stores all your most important files in one secure location. Access them whenever you need, share and collaborate with friends, family and co-workers.</p>
         <Button text="Get Started" className="px-20"/>
